3ce1d29667634740777e64f9d5f83f33a3dcef54
apignet <apignet@equancy.com>
2020-07-13 09:07:32 +0000

    initial commit

---
+++ ckks_titanic/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Resolving the titanic datascience problem using homomorphic encryption, implemented with the CKKS schem',
+    author='apignet',
+    license='Equancy',
+)
