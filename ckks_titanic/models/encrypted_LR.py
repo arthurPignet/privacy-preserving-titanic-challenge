a4b1ac0d42f358791a2cebdb27d1cbd882d95a17
apignet <apignet@equancy.com>
2020-07-15 13:42:53 +0200

    add ML models

--- ckks_titanic/models/encrypted_LR.py
+++ ckks_titanic/models/encrypted_LR.py
@@ -1,19 +1,8 @@
-import time
+import logging
 
 import numpy as np
 import tenseal as ts
 
-from sklearn.model_selection import train_test_split
-
-
-
-
-
-def crytp_array(X, local_context):
-    res = []
-    for vec in X:
-        res.append(ts.ckks_vector(local_context, vec))
-    return res
 
 
 class LogisticRegressionHE:
@@ -23,13 +12,13 @@ class LogisticRegressionHE:
         if not safety:
             self.DIR_NE = []
             self.CRITERE_NE = []
-            print("Warning :  The data will be decrypted during the process, the protocol is not safe")
+            logging.critical(" The data will be decrypted during the process, the protocol is not safe")
             assert local_context is not None, "The protocol chosen (safety is set to false) need the context to refresh and decrypt ciphertext"
         self.safety = safety
         self.num_iter = num_iter
         self.verbose = verbose
-        self.CRITERE = []
-        self.NGRADIENT = []
+        self.loss = []
+        self.grad_norm = []
         self.weight = None
         self.weight_ne = None
         self.bias = None
@@ -101,12 +90,12 @@ class LogisticRegressionHE:
         self.bias = ts.ckks_vector(self.context, self.bias_ne)
         self.bias_ne = np.array(self.bias_ne)
         # initial values
-        for i in range(len(X)):
-            print(X[i].decrypt())
-        print(X_ne)
-        for i in range(len(Y)):
-            print(Y[i].decrypt())
-        print(Y_ne)
+        # for i in range(len(X)):
+        #     print(X[i].decrypt())
+        # print(X_ne)
+        # for i in range(len(Y)):
+        #     print(Y[i].decrypt())
+        # print(Y_ne)
         encrypted_prediction = self.forward(X)  # we can add batching later
         prediction = self.forward_ne(X_ne)
         direction_weight, direction_bias = self.backward(X, encrypted_prediction, Y)
@@ -116,12 +105,12 @@ class LogisticRegressionHE:
         ne_loss += (self.reg_para / 2) * self.weight_ne.dot(self.weight_ne)
         self.CRITERE_NE.append(ne_loss)
         self.DIR_NE.append((ne_direction_weight, ne_direction_bias))
-        self.NGRADIENT.append((direction_weight.decrypt(), direction_bias.decrypt()))
-        print("encrypted grad")
-        print(direction_weight.decrypt())
-        print('Unencrypted grad')
-        print(ne_direction_weight)
-        print(
+        self.grad_norm.append((direction_weight.decrypt(), direction_bias.decrypt()))
+        logging.debug("encrypted grad")
+        logging.debug(direction_weight.decrypt())
+        logging.debug('Unencrypted grad')
+        logging.debug(ne_direction_weight)
+        logging.debug(
             "error %d" % (np.sum(np.power((np.array(direction_weight.decrypt()) - ne_direction_weight), 2)) / np.sum(
                 np.power(ne_direction_weight, 2))))
 
@@ -130,7 +119,6 @@ class LogisticRegressionHE:
                 self.weight = self.refresh(self.weight)
                 self.bias = self.refresh(self.bias)
                 # refreshing the weight and the bias to avoid scale out of bound
-                # TODO FIX UNSECURED PROTOCOL
             encrypted_prediction = self.forward(X)  # we can add batching later
             prediction = self.forward_ne(X_ne)
             direction_weight, direction_bias = self.backward(X, encrypted_prediction, Y)
@@ -141,19 +129,19 @@ class LogisticRegressionHE:
             self.bias_ne -= ne_direction_bias
             self.iter += 1
             if self.verbose and self.iter % 1 == 0:
-                print("iteration number %d is starting" % self.iter)
+                logging.info("iteration number %d is starting" % self.iter)
                 if not self.safety:
                     ne_loss = np.log(prediction).dot(Y_ne)
                     ne_loss += (1 - np.array(Y_ne)).T.dot(np.log(1 - np.array(prediction)))
                     ne_loss += (self.reg_para / 2) * self.weight_ne.dot(self.weight_ne)
                     self.CRITERE_NE.append(ne_loss)
                     self.DIR_NE.append((ne_direction_weight, ne_direction_bias))
-                    self.NGRADIENT.append((direction_weight.decrypt(), direction_bias.decrypt()))
-                    print("encrypted grad")
-                    print(direction_weight.decrypt())
-                    print('Unencrypted grad')
-                    print(ne_direction_weight)
-                    print(
+                    self.grad_norm.append((direction_weight.decrypt(), direction_bias.decrypt()))
+                    logging.debug("encrypted grad")
+                    logging.debug(direction_weight.decrypt())
+                    logging.debug('Unencrypted grad')
+                    logging.debug(ne_direction_weight)
+                    logging.debug(
                         "error %d" % (np.sum(
                             np.power((np.array(direction_weight.decrypt()) - ne_direction_weight), 2)) / np.sum(
                             np.power(ne_direction_weight, 2))))
@@ -177,51 +165,3 @@ class LogisticRegressionHE:
 
     def __call__(self, *args, **kwargs):
         return self.forward(*args, **kwargs)
-
-#TODO mettre tout ca dans un notebook à part
-if __name__ == "__main__":
-    print("Uploading and preprocessing the data")
-    # paths
-    # data_path = "./data/" #whole data set
-    data_path = "./data_quick_demo/"  # subset of the data set, with 15 train_samples and 5 test_samples
-    # loading and prepossessing of the data
-    raw_train, raw_test = preprocessing.data_import(data_path)
-    train, submission_test = preprocessing.preprocessing(raw_train, raw_test)
-    # splitting in train (85%) and test(15%) sets
-    train, test = train_test_split(train, test_size=0.15)
-    train_labels = train.Survived
-    test_labels = test.Survived
-    train_features = train.drop("Survived", axis=1)
-    test_features = test.drop("Survived", axis=1)
-    # adding bias
-
-    print("Definition of safety parameters")
-    timer = time.time()
-    # context = ts.context(ts.SCHEME_TYPE.CKKS, 32768,
-    # coeff_mod_bit_sizes=[60, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 60])
-
-    context = ts.context(ts.SCHEME_TYPE.CKKS, 16384, coeff_mod_bit_sizes=[60, 40, 40, 40, 40, 40, 40, 40, 60])
-    # context = ts.context(ts.SCHEME_TYPE.CKKS, 8192, coeff_mod_bit_sizes=[40, 21, 21, 21, 21, 21, 21, 40])
-    context.global_scale = pow(2, 40)
-    print("Done. " + str(time.time() - timer) + " seconds")
-    print('Generation of Galois Key...')
-    timer = time.time()
-    context.generate_galois_keys()
-    print("Done. " + str(time.time() - timer) + " seconds")
-    print("Data encryption...")
-    timer = time.time()
-    encrypted_X = crytp_array(train_features.to_numpy(), context)
-    encrypted_Y = crytp_array(train_labels.to_numpy().reshape((-1, 1)), context)
-    encrypted_test_X = crytp_array(test_features.to_numpy(), context)
-    encrypted_test_Y = crytp_array(test_labels.to_numpy().reshape((-1, 1)), context)
-    print("Done. " + str(time.time() - timer) + " seconds")
-    print("Training starting")
-    model = LogisticRegressionHE(local_context=context, safety=False, verbose=True, num_iter=40)
-    model.fit(encrypted_X, encrypted_Y, X_ne=train_features.to_numpy(), Y_ne=train_labels.to_numpy().reshape((-1, 1)))
-    print("Training done. " + str(time.time() - timer) + " seconds")
-    model.weight = ts.ckks_vector(model.context, model.weight.decrypt())
-    for x, y in zip(encrypted_test_X, encrypted_test_Y):
-        print("label : " + str(y.decrypt()[0]))
-        print("predict : " + str(model.predict(x).decrypt()))
-    acc = model.encrypted_accuracy(encrypted_test_X, encrypted_test_Y)
-    print("Accuracy : %s " % acc[0])
